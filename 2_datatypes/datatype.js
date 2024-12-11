abfd6bc247acdcacb607dbe45a712ae63e2b9fa7
DINESHLINGAM-2909 <dineshipcc47@gmail.com>
2024-12-11 11:56:17 +0530

    new files added

--- 2_datatypes/datatype.js
+++ 2_datatypes/datatype.js
@@ -1,7 +1,7 @@
 //! Datatypes in javaScript
 // 1.primitive datatype
 
-//1.Number
+//!1.Number
 var a = 10
 console.log(a)
 console.log(typeof a) //number
@@ -14,7 +14,7 @@ let c = Number(20) //number method
 console.log(c)
 console.log(typeof c) //number
 
-//2.boolean
+//!2.boolean
 let d = true
 console.log(d)
 console.log(typeof d)//boolean
@@ -45,3 +45,44 @@ console.log(typeof s3)
 let sob = new String("dinesh");
 console.log(sob)
 console.log(typeof sob)
+
+//! 4.undedefined - we declare the variable and we not initialize it
+
+let a1
+console.log(a1) //undefined
+console.log(typeof a1)
+
+//! 5.null - the tyoe of null will be object
+let a2 = null
+console.log(a2)
+console.log(typeof a2) //object
+
+//BigInt - It will add the n in suffix of the number
+let a3 = BigInt(43)
+console.log(a3)
+console.log(typeof a3) //BigInt
+
+// ? 2.Non-primive datatypes
+
+//! 1.Array - you can give any kind of data 
+
+let arr = [1,'Hi',true,null,undefined]
+console.log(arr)
+
+//! 2.function - set of statements which present in same blocks
+
+function add(){
+    console.log("This is add function")
+} 
+
+add()
+
+// ! 3.object - anything have physical existance is called objects
+
+let student = {
+    sname:"dinesh",
+    sid:"101",
+    phone:9198,
+    skills:['html','css','js']
+}
+console.log(student)
---
+++ 3_string/string.js
@@ -0,0 +1,113 @@
+
+// ! How to declare string?
+
+let str1 = "hello"
+let str2 = `HELLO`
+let str3 = 'hello'
+
+// ! how to know the length of the string
+
+let length = str1.length // length is the property
+console.log(length)
+
+// ! String methods
+
+// ? 1.toUpperCase() 
+//it is used to convert the string into uppercase and it will return one new string
+
+
+let upper = str1.toUpperCase()
+console.log(upper) //HELLO
+console.log(str1)  //hello (it will not change the original string)
+
+
+// ?  2.toLowerCase() 
+ //it is used to convert the string into lower case and it will return one new string
+
+let lower = str2.toLowerCase();
+console.log(str2) //HELLo
+console.log(lower) //hello (it will not change the original string)
+
+// ?  3.indexOf() 
+// it is used to give the index of particular character
+
+// it will take the first occurance of the character
+
+let str4 = "hello how are you"
+console.log(str4.indexOf('o'))
+
+// ? 4.charAt(index) 
+// it will give the character based on its index value
+
+console.log(str4.charAt(4)) // o
+
+// ? 5.concat()
+// it is used to combine two or more than 2 strings and it will return one string.
+
+let str5 = 'hello'
+let str6 = 'how are you'
+
+let combined = str5.concat(" ",str6," dinesh")
+console.log(combined) //hello how are you dinesh
+
+// ? 6.trim()
+// trim() is used for remove the spaces by both sides in the string and it will return new string.
+let str7 = "   dinesh     "
+console.log(str7) //   dinesh   
+console.log(str7.length) //14
+
+let str8 = str7.trim()
+console.log(str8) //dinesh
+console.log(str8.length) //6
+
+// ? 7.includes(substring of original string)
+// it is used to check whether the particular character or words present in the string or not.
+// if it is present it will return true, else return false. str7 = "dinesh "
+
+console.log(str7.includes('n')) //true
+console.log(str7.includes('o')) //false
+
+// ? 8.statsWith(string)
+// it is used to check the string is start with the particular character
+
+console.log(str8.startsWith('d'))  //true
+
+// ? 9.endsWith(string)
+// it is used to check the string is end with the particular character
+
+console.log(str8.endsWith('s')) //false
+
+// ? 10.split(string)
+ // it is use to convert any string into array.
+
+let str9 = "I am dinesh"
+
+let arr = str9.split(" ")
+console.log(arr) //(3) ['I', 'am', 'dinesh']
+
+// ? 11.slice(start index,end index)
+ // it is used to extract the charater or string from another string
+ // it will take two parameters start index and end index.
+ // it will not includes the end index value. 
+ // if you not give the end index it from start index it will give all the remaining string.
+let str10 = 'hi, this is dinesh'
+let str11 = str10.slice(4,18); 
+console.log(str11) // this is dinesh
+console.log(str11.slice(2)) //is is dinesh
+// we can give negative value as an index it will take from the last.
+console.log(str11.slice(-3,-1))  //es
+console.log(str11.slice(-3)) //esh
+console.log(str11.slice(4,0)) //output will be empty we can't give start index greater than end index
+ 
+// ? 12.subString(start index,end index)
+ // it is used to extract the charater or string from another string
+ // it will take two parameters start index and end index.
+ // if you give negative value it will convert it is a zero.
+ // if you give start index > end index it will swap it
+ // substring(7,1) -> substring(1,7)
+
+ console.log(str11.substring(-4,6)) //this i
+
+ console.log(str11.substring(7,1)) //his is
+
+
