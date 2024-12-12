371546714864dd891c4c8bb7a5827b133efcc730
DINESHLINGAM-2909 <dineshipcc47@gmail.com>
2024-12-12 16:46:27 +0530

    new files added

--- 3_string/string.js
+++ 3_string/string.js
@@ -106,8 +106,26 @@ console.log(str11.slice(4,0)) //output will be empty we can't give start index g
  // if you give start index > end index it will swap it
  // substring(7,1) -> substring(1,7)
 
+
  console.log(str11.substring(-4,6)) //this i
 
- console.log(str11.substring(7,1)) //his is
+ console.log(str11.substring(7,1)) //his i
 
+//! Palindrome program
+let s = "madame"
+let temp = ""
+for(let i = s.length-1;i>=0;i--)
+    temp += s.charAt(i)
+if(s==temp)
+    console.log("palindrome")
+else
+    console.log("Not a palindrome")
 
+let s1= 'madam'
+let dup = ''
+for(let i = 0;i<s1.length;i++)
+    if(!dup.includes(s1.charAt(i)))
+        dup+=s1.charAt(i);
+console.log(dup)
+
+//1010 - 10 8+0+2+0 = 10
